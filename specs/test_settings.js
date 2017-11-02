bdda6207d1ef0ebb4d13fb30b8d480a5e9b94c60
JortvD <jortvandriel@hotmail.com>
2017-11-02 17:12:07 +0100

    Added a test

---
+++ specs/test_settings.js
@@ -0,0 +1,20 @@
+var settings = require("../index")();
+
+console.log(settings.name + " (" + settings.version + ")");
+
+settings.settings().setting("key", "value1");
+console.log("Test1: " + (settings.settings().key() === "value1"));
+
+settings.settings().setting("key1.key2", "value2");
+console.log("Test2: " + (settings.settings().key1().key2() === "value2"));
+
+settings.settings().setting("key1.key3", "value3");
+console.log("Test3: " + (settings.settings().key1().key3() === "value3"));
+
+settings.settings().setting("key4.key5.key6", "value4");
+console.log("Test4: " + (settings.settings().key4().key5().key6() === "value4"));
+
+settings.settings().setting("key4.key5.key7", "value5");
+console.log("Test5: " + (settings.settings().key4().key5().key7() === "value5"));
+
+console.log(settings.settings().key4().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5().key5());
